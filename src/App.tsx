5fcf9f1d089aaf657f3fe86bcb3d7a6d8e10728c
OxygenO22 <bamaik@mail.ru>
2023-09-26 07:51:39 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { Book, BookInformation } from "./lib/types";
+import { getBooks, getUsers, getReviews } from "./lib/api";
+import { useEffect, useState, FC } from "react";
+import Card from "./Card";
+
+const toBookInformation = (book: Book): BookInformation => {
+  return {
+    id: book.id,
+    name: book.name || "Книга без названия",
+    author: { name: "Test Author", id: "id" },
+    reviews: [
+      { id: "test", text: "test text", user: { id: "sdf", name: "Reviewer" } },
+    ],
+    description: book.description,
+  };
+};
+
+const App: FC = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      setIsLoading(true);
+      const fetchedBooks = await getBooks();
+      setBooks(fetchedBooks);
+      setIsLoading(false);
+    };
+    fetchBooks();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Мои книги:</h1>
+      {isLoading && <div>Загрузка...</div>}
+      {!isLoading &&
+        books.map((b) => <Card key={b.id} book={toBookInformation(b)} />)}
     </div>
   );
-}
+};
 
 export default App;
